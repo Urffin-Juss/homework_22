df976a25dcc36f2ec57e6b32f6460feb3bb7a437
Помогаев Ярослав <rickpomogaev@Air-Pomogaev.bbrouter>
2026-01-06 16:19:12 +0300

    lose secret key

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 
-
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 DEBUG = True
