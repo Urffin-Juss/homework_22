6dacf3b00917ab991abd0a055212212683413042
Помогаев Ярослав <rickpomogaev@MacBook-Air-Pomogaev.local>
2026-01-08 14:14:48 +0300

    to send

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,12 +3,12 @@ from catalog.apps import CatalogConfig
 from . import views
 
 
-app_name = CatalogConfig.name
+app_name = 'catalog'
 
 urlpatterns = [
     path('',views.home,name='home'),
     path('contacts',views.contacts,name='contacts'),
-    path('products/<int:product_id>',views.product_detail,name='product_detail'),
+    path('products/<int:product_id>', views.product_detail, name='product_detail'),
 
 
 ]
