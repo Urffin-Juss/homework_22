cd6945e3ba8d04e34a3b41064e706a2206040fa7
Помогаев Ярослав <rickpomogaev@MacBook-Air-Pomogaev.local>
2026-01-08 13:17:58 +0300

    lets the party started

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,21 @@
+from gc import get_objects
+
 from django.shortcuts import render
+from catalog.models import Product
 
 def home(request):
     return render(request, 'catalog/home.html')
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
+
+def product_details(request, product_id):
+    product = get_objects_or_404(Product, id=product_id)
+    context = {
+        'product': product
+        'title': f'{product.name} - Детали'
+
+        }
+    return render(request, 'catalog/product_detail.html', context)
+
+
