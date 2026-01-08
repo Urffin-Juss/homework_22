6095429018ad4f28001f8cee566ee64003e64256
Помогаев Ярослав <rickpomogaev@MacBook-Air-Pomogaev.local>
2026-01-08 13:18:44 +0300

    refresh home

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,22 @@ from gc import get_objects
 from django.shortcuts import render
 from catalog.models import Product
 
+
 def home(request):
-    return render(request, 'catalog/home.html')
+    """Главная страница со списком товаров"""
+    products = Product.objects.all()[:12]  # Ограничим 12 товарами
+    for product in products:
+        # Обрезаем описание для превью
+        if product.description and len(product.description) > 100:
+            product.description_preview = product.description[:100] + '...'
+        else:
+            product.description_preview = product.description or ''
+
+    context = {
+        'products': products,
+        'title': 'Главная страница'
+    }
+    return render(request, 'catalog/home.html', context)
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
