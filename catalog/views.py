c857a3f566495944bcaea14ef2e4ea67d3e5b4f6
Помогаев Ярослав <rickpomogaev@MacBook-Air-Pomogaev.local>
2026-01-09 10:34:07 +0300

    fix syntax

--- catalog/views.py
+++ catalog/views.py
@@ -16,15 +16,15 @@ class HomeView(ListView):
         context['title'] = 'Главная страница'
         return context
 
-    class ContactsView(TemplateView):
-        template_name = 'catalog/contacts.html'
+class ContactsView(TemplateView):
+    template_name = 'catalog/contacts.html'
 
-    class ProductDetailView(DetailView):
+class ProductDetailView(DetailView):
 
-        model = Product
-        template_name = 'catalog/product_detail.html'
-        context_object_name = 'product'
-        pk_url_kwarg = 'product_id'
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    context_object_name = 'product'
+    pk_url_kwarg = 'product_id'
 
         def get_context_data(self, **kwargs):
             context = super().get_context_data(**kwargs)
