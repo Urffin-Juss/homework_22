9ac9c336686f5de3f373913e717a30f586803523
Помогаев Ярослав <rickpomogaev@MacBook-Air-Pomogaev.local>
2026-01-09 10:18:37 +0300

    procces

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField("Заголовок", max_length=200)
+    content = models.TextField("Содержимое")
+    preview = models.ImageField("Превью", upload_to="blog/preview/", blank=True, null=True)
+    created_at = models.DateTimeField("Дата создания", auto_now_add=True)
+    is_published = models.BooleanField("Опубликовано", default=False)
+    views_count = models.IntegerField("Просмотры", default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self) -> str:
+        return self.title
+
