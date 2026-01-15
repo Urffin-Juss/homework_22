0aaa64926af4e6880a82165f70d7d82f916cb70c
Urffin-Juss <im@maxsuns.ru>
2026-01-15 14:35:04 +0300

    make losk

--- users/urls.py
+++ users/urls.py
@@ -4,8 +4,8 @@ from .views import UserCreateView, UserLogoutView, UserLoginView
 app_name = 'users'
 
 urlpatterns = [
-    path('user/register/', UserCreateView.as_view(), name='register'),
-    path('user/login/', UserLoginView.as_view(), name='login'),
-    path('user/logout/', UserLogoutView.as_view(), name='logout'),
+    path('register/', UserCreateView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 
 ]
